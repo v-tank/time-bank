ed58b52006ccbf3a6427c702f08eeffce1033fcd
Stacy Areas <stacyareas@gmail.com>
2018-03-03 18:18:38 -0800

    set handlebars

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Static directory
 app.use(express.static("public"));
 
