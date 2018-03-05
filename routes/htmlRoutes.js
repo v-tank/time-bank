8ffe879d241ad74a405d0f7ebc073e0cb9330758
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-05 10:33:38 -0800

    working hashes and db connection

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,34 +1,39 @@
 var path = require("path");
 
-module.exports = function(app, passport) {
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/index.handlebars"));
+module.exports = function (app, passport) {
+  app.get("/", function (req, res) {
+    res.render("index");
   });
 
-  app.get("/signup", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/signup.handlebars"));
+  app.get("/signup", function (req, res) {
+    res.render("signup");
   });
 
-  app.get("/profile", isLoggedIn, function(req, res) {
-  	res.sendFile(path.join(__dirname, "../views/profile.handlebars"));
+  app.get("/login", function (req, res) {
+    res.render("login");
   });
 
-  app.get("/logout", function(req, res) {
-  	req.logout();
-  	res.redirect("/");
-  });
+  // app.get("/profile", isLoggedIn, function(req, res) {
+  // 	res.render("profile");
+  // });
 
-  app.post("/signup", passport.authenticate("local-signup", {
-    successRedirect: "/profile",
-    faliureRedirect: "/signup",
-    faliureFlash: true
-  }));
+  // app.get("/logout", function(req, res) {
+  // 	req.logout();
+  // 	res.redirect("/");
+  // });
+
+  // app.post("/signup", passport.authenticate("local", {  	
+
+  //   successRedirect: "/login",
+  //   faliureRedirect: "/signup",
+  //   faliureFlash: true
+  // }));
 }
 
-function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated()) {
- 		return next()
- 	} else {
- 		return res.redirect("/");
- 	}
-}
+// function isLoggedIn(req, res, next) {
+// 	if (req.isAuthenticated()) {
+//  		return next()
+//  	} else {
+//  		return res.redirect("/");
+//  	}
+// };
