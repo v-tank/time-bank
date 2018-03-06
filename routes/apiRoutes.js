abcb8020868b34f874b712abed2217c263a90d2b
Payam <payamasem@berkeley.edu>
2018-03-05 17:13:48 -0800

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -44,93 +44,6 @@ module.exports = function (app) {
     });
   });
 
-<<<<<<< HEAD
-
-
- // POST for adding new child
- app.post('/child', function(req, res) {
-  var name = req.body.name;
-  db.Parents.create({
-    name: name,
-  })
-    .then(function(dbParent) {
-      console.log(dbParent);
-      res.json(dbParent);
-    })
-});
-
- // DELETE for deleting child
- app.delete('/child/:id', function(req, res) {
-  var id = req.params.id;
-  db.Parents.destroy({
-    where: { id: id }
-  })
-    .then(function(dbParent) {
-      console.log(dbParent);
-      res.json(dbParent);
-    });
-});
-
-
-  // GET one child by id
-  app.get('/child/:id', function(req, res) {
-    var id = req.params.id;
-    db.Parents.find({
-      where: { id: id }
-    })
-      .then(function(dbParent) {
-        console.log(dbParent);
-        res.json(dbParent);
-      });
-  });
-
-
-
- // POST for adding new task
- app.post('/task', function(req, res) {
-  var taskname = req.body.taskname;
-  db.Parents.create({
-    taskname: taskname,
-  })
-    .then(function(dbParent) {
-      console.log(dbParent);
-      res.json(dbParent);
-    })
-});
-
-  // GET all tasks
-  app.get('/tasks', function(req, res) {
-    db.Parent.findAll()
-      .then(function(dbParent)  {
-        res.json(dbParent);
-      });
-  });
-
-  // GET one task by id
-  app.get('/task/:id', function(req, res) {
-    var id = req.params.id;
-    db.Parents.find({
-      where: { id: id }
-    })
-      .then(function(dbParent) {
-        console.log(dbParent);
-        res.json(dbParent);
-      });
-  });
-
- // DELETE a task
- app.delete('/task/:id', function(req, res) {
-  var id = req.params.id;
-  db.Parents.destroy({
-    where: { id: id }
-  })
-    .then(function(dbParent) {
-      console.log(dbParent);
-      res.json(dbParent);
-    });
-});
-};
-=======
   // Route for logging user out
   app.get("/logout", function (req, res) {
     req.logout();
@@ -153,4 +66,3 @@ module.exports = function (app) {
     }
   });
 }
->>>>>>> 3f1e9f2889bce6ba66aa5509399aa0f2442112c6
