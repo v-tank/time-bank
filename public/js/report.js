2754b35ead3f6a1cad1eea0cb41b84d51541593f
NI GUO <guoni0909@gmail.com>
2018-03-07 11:17:15 -0800

    updated report.js file

--- public/js/report.js
+++ public/js/report.js
@@ -1,7 +1,28 @@
 $(document).ready(function(){
-	$("#add-child-submit").on("click",function(event) {
-		event.prevenDefault();
+	var tasksCanvas = document.getElementById("tasksChart");
 
+	Chart.defaults.global.defaultFontFamily = "Lato";
+	Chart.defaults.global.defaultFontSize = 18;
 
+	var tasksData = {
+	    labels: [
+	      "Activity",
+	      "Reading"
+	    ],
+	    datasets: [
+	      {
+	      	data: [30, 70],
+	        // data: {{productive_time}},
+	        backgroundColor: [
+	            "#FF6384",
+	            "#63FF84"
+	        ]
+	      }]
+	};
+
+	var pieChart = new Chart(tasksCanvas, {
+	  type: 'pie',
+	  data: tasksData
 	});
+
 });
