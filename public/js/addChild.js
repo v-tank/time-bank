c1ed13ef21e53c13c84b5eda56893b1ed4cc93df
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-06 22:53:30 -0800

    initial commit

---
+++ public/js/addChild.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  var childName = $("#childName").val().trim();
+  var exerciseMultipler = $("#exercise_multiplier").val().trim();
+  var readingMultiplier = $("#reading_multiplier").val().trim();
+
+  
+});
