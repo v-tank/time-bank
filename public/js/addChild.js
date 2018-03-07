171640199919132f4bb7961d57625257909e3b1e
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-07 15:51:19 -0800

    working form with ajax POST and GET

--- public/js/addChild.js
+++ public/js/addChild.js
@@ -1,7 +1,45 @@
 $(document).ready(function() {
-  var childName = $("#childName").val().trim();
-  var exerciseMultipler = $("#exercise_multiplier").val().trim();
-  var readingMultiplier = $("#reading_multiplier").val().trim();
+  getChildren();
 
+  $("#add-child-submit").on("click", function(event) {
+    event.preventDefault();
+    var childName = $("#childName").val().trim();
+    var exerciseMultipler = $("#exercise_multiplier").val().trim();
+    var readingMultiplier = $("#reading_multiplier").val().trim();
+    var arr = [];
+
+    var obj = {
+      name: childName,
+      exercise: (exerciseMultipler)/100,
+      reading: (readingMultiplier)/100,
+      array: arr
+    }
+
+    for (property in obj) {
+      console.log(property); // Outputs: foo, fiz or fiz, foo
+      arr.push(property);
+    }
+
+    console.log(arr);
+
+
+    $.post("/addChild", obj, function() {
+      console.log("Object sent.");
+    });
+
+    getChildren();
+  });
   
+  function getChildren() {
+    $.get("/children", function(res) {
+      console.log(res);
+      // <a class="dropdown-item" href="#">Action</a>
+      $("#dropdown").empty();
+
+      for (var i = 0; i < res.length; i++) {
+        var a = '<option value="' + res[i].id + '">' + res[i].name + '</option>';
+        $("#dropdown").append(a);
+      }
+    });
+  }
 });
