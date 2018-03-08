01971d23ae830e8b533cedf5bc30ff5e1b7ab73b
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-08 12:36:27 -0800

    earnit route not working

--- public/js/pickAChild.js
+++ public/js/pickAChild.js
@@ -2,11 +2,21 @@ $(document).ready(function() {
 
   $("#earnit-img").on("click", function() {
     var childId = $("#dropdown").val();
-    console.log(childId);
 
-    $.get("/earnIt/" + childId, function(res) {
-      // console.log(res);
-    })
+    // alert(childId);
+
+    //  grab child id
+    // var Newlocation = "/earnit/"+childId;
+    var url = "/earnIt/" + childId;
+    // alert(url);
+    // debugger;
+    
+    window.location.href = url;
+
+    // $.get('/earnIt/' + childId + "/");
+    // window.location.reload(url);
+
+    
   })
   
   
