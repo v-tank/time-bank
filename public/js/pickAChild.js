2f320a91d6c2570bde17a5a74386a56396fe75a1
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-08 14:48:01 -0800

    grabs child ID to redirect to corresponding pages

--- public/js/pickAChild.js
+++ public/js/pickAChild.js
@@ -1,24 +1,12 @@
 $(document).ready(function() {
 
-  $("#earnit-img").on("click", function() {
+  $("#earnit-img").on("click", function(event) {
+    event.preventDefault();
+
     var childId = $("#dropdown").val();
 
-    // alert(childId);
-
-    //  grab child id
-    // var Newlocation = "/earnit/"+childId;
     var url = "/earnIt/" + childId;
-    // alert(url);
-    // debugger;
     
     window.location.href = url;
-
-    // $.get('/earnIt/' + childId + "/");
-    // window.location.reload(url);
-
-    
-  })
-  
-  
-
+  });
 });
