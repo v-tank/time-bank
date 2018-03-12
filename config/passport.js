4d2ba1308fda3b64884da679f9488d970a1e2735
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-12 12:40:43 -0700

    add comments

--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,11 @@
+// Require necessary packages and files
 var passport = require("passport");
 var LocalStrategy = require("passport-local").Strategy;
 var db = require("../models");
+
 // Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
 passport.use(new LocalStrategy(
-  // Our user will sign in using an name, rather than a "username"
+  // Our user will sign in using a username
   {
     usernameField: "name",
   },
@@ -32,6 +34,7 @@ passport.use(new LocalStrategy(
     });
   }
 ));
+
 // In order to help keep authentication state across HTTP requests,
 // Sequelize needs to serialize and deserialize the user
 // Just consider this part boilerplate needed to make it all work
