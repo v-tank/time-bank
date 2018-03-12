846cbe0effdf8a079d9ea5ac2c4c3990c664eb39
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-12 15:26:03 -0700

    added comments

--- public/js/addChild.js
+++ public/js/addChild.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-   getChildren();
+   getChildren(); // Fetches children upon start
 
   $("#add-child-submit").on("click", function(event) {
     event.preventDefault();
+
+    // Grabs the form values
     var childName = $("#childName").val().trim();
     var exerciseMultipler = $("#exercise_multiplier").val().trim();
     var readingMultiplier = $("#reading_multiplier").val().trim();
     var arr = [];
 
+    // Creates an object from the form values
     var obj = {
       name: childName,
       exercise: (exerciseMultipler)/100,
@@ -16,26 +19,28 @@ $(document).ready(function() {
     }
 
     for (property in obj) {
-      console.log(property); // Outputs: foo, fiz or fiz, foo
+      console.log(property); 
       arr.push(property);
     }
 
     console.log(arr);
 
-
+    // Posts the object to the route and adds to the database
     $.post("/addChild", obj, function() {
       console.log("Object sent.");
     });
 
-    getChildren();
+    getChildren(); // Refreshes the children after a child is added to the database
   });
   
+  // Function make a GET call to grab children from the database and add them to a dropdown menu
   function getChildren() {
     $.get("/children", function(res) {
       console.log(res);
       // <a class="dropdown-item" href="#">Action</a>
       $("#dropdown").empty();
 
+      // Loop through the returned array of results and dynamically add the children to the dropdown
       for (var i = 0; i < res.length; i++) {
         var a = '<option value="' + res[i].id + '">' + res[i].name + '</option>';
         $("#dropdown").append(a);
